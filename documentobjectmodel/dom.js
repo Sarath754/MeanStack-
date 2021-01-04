fc5a106cc3097a45dfce09545494b3f731eb41c6
Sarath Raj <sarathraj153@gmail.com>
2021-01-04 15:19:28 +0530

    document object model

---
+++ documentobjectmodel/dom.js
@@ -0,0 +1,49 @@
+// var heads=document.getElementsByTagName("h1")
+
+//     for(head of heads){
+
+//         head.style.color="red";
+    
+//     }
+
+
+// var hd=document.getElementById("one")
+
+// hd.style.color="cyan"
+
+// var hd=document.getElementById("two")
+
+// hd.style.color="blue"
+
+// var lis=document.getElementsByClassName("lis");
+
+// for(head of lis){
+
+//     head.style.color="green";
+// }
+
+
+// var dm=document.getElementById("dm")
+
+// dm.textContent="DocumentObjectMethods"
+// dm.innerHTML="<em>Documentobjectmethods</em>" //italics font varan
+
+
+// var h1=document.getElementById("one")
+
+// alert(h1.textContent)
+ 
+
+var lis=document.querySelectorAll("li");
+
+lis.foreach(li=>li.style.color="green")
+
+
+var ls=document.querySelectorAll(".lis")
+
+ls.foreach(l=>l.style.color="red")
+
+
+var hd=document.querySelector("#one")
+
+hd.style.color="green"
---
+++ documentobjectmodel/domeve.js
@@ -0,0 +1,9 @@
+var cl=document.querySelector("#clk");
+
+cl.addEventListener("click",()=>{
+
+    cl.textContent="clicked";
+
+    cl.style.color="red";
+
+})
